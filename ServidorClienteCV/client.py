c5355fe4073380ede4d2c0158327dcb43269c773
guilherme3987 <guilhermesmt3987@gmail.com>
2024-10-24 09:37:36 -0300

    Pasta com arquivos server e client com a visualização dos frames com a biblioteca OpenCV

--- ServidorClienteCV/client.py
+++ ServidorClienteCV/client.py
@@ -7,9 +7,7 @@ DEST_IP = '127.0.0.1'  # IP do servidor (localhost para teste)
 DEST_PORT = 5004        # Porta do servidor
 VIDEO_PATH = "C:/Users/guilh/OneDrive/Área de Trabalho/Redes/UPD_Video/meme.mp4"  # Caminho do vídeo a ser enviado
 
-# Função para enviar dados para o servidor
 def send_video(video_path):
-    # Cria um socket UDP
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # Abre o vídeo usando PyAV
@@ -19,7 +17,7 @@ def send_video(video_path):
         # Converte o quadro para bytes
         payload = frame.to_ndarray().tobytes()
         
-        # Fragmenta o payload se necessário
+        # Fragmenta o payload 
         max_packet_size = 1400  # Tamanho máximo para o pacote
         for i in range(0, len(payload), max_packet_size):
             packet = payload[i:i + max_packet_size]
